60479d1c0e3835cc4b3af90630b03a1d682aad03
alvdelci <delcimarioalvesdev@gmail.com>
2024-11-18 15:41:35 -0300

    refactor: Configurando novos modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PostsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
