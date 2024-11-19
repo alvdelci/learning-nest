6983532366faff54ca9a2e6ccf799eaf971f2590
alvdelci <delcimarioalvesdev@gmail.com>
2024-11-18 21:16:43 -0300

    feat: Criando entidade de Users

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Post } from 'src/posts/entities/post.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  age: number;
+
+  @OneToMany(() => Post, (post) => post.id)
+  posts: Post[];
+}
