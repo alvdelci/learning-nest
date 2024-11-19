adc7599dd2771695bd300f75dea94313d3cbd533
alvdelci <delcimarioalvesdev@gmail.com>
2024-11-18 21:16:26 -0300

    feat: Criando entidade de Posts

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,23 @@
-export class Post {}
+import { User } from 'src/users/entities/user.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  likes: number;
+
+  @ManyToOne(() => User, (user) => user.id)
+  user: User;
+}
