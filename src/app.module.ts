df1935c643b2eb73d7ec92abdba9435a5ab592cc
alvdelci <delcimarioalvesdev@gmail.com>
2024-11-18 21:15:37 -0300

    refactor: Adicionando importacao do database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AppController } from './app.controller';
-
+import { DatabaseModule } from './database/database.module';
 @Module({
-  imports: [UsersModule, PostsModule],
+  imports: [DatabaseModule, UsersModule, PostsModule],
   controllers: [AppController],
   providers: [],
 })
